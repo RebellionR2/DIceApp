b0676c50e88d89cdce8f51bea97d83a97f6ed237
arsenal-game <arsenalgame579@gmail.com>
2024-09-14 17:50:10 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Center(child: Text('Diceeeee')),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +15,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  //const DicePage({Key? key}) : super(key: key);
+   int leftDice=1;
+   int rightDice=1;
+
+   void changeDiceFace(){
+     leftDice=Random().nextInt(6)+1;
+     rightDice=Random().nextInt(6)+1;
+   }
   @override
   Widget build(BuildContext context) {
-    return Container();
+     
+
+    return Center(
+      child: Row(
+        
+        children: [
+          Expanded(child:TextButton(
+            onPressed: (){
+              
+              setState(() {
+                 changeDiceFace();  
+                
+              });
+            },
+            child: Image.asset('images/dice$leftDice.png'))),
+          Expanded(child:TextButton(
+            onPressed: (){
+              
+              setState(() {
+                changeDiceFace();
+              });
+
+            },
+            child: Image.asset('images/dice$rightDice.png')))
+        ],
+      ),
+    );
   }
 }
+
+
